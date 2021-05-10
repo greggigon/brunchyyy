cbd163427b93ccab8418a9b9f7a6bcbc58844330
greggigon <greg.gigon@gmail.com>
2021-05-10 15:48:20 +0100

    Upgraded some libraries

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ module.exports = {
     libraryTarget: 'umd'
   },
   resolve: {
-    mainFields: ["main", "module"],
+    mainFields: ['main', 'module'],
     alias: {
       'universal-user-agent': path.resolve(__dirname, './node_modules/universal-user-agent/dist-node/')
     }
